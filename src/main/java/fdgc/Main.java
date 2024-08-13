6f6129eb5d58567cb438938bd660972f1691a173
XaxisPlayz <66885524+Fotohh@users.noreply.github.com>
2024-08-13 15:10:02 -0700

    added installer data in

--- src/main/java/fdgc/Main.java
+++ src/main/java/fdgc/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Window window = new Window();
         window.load(args);
+
+        //jpackage --type exe --app-version "2.0" --input . --dest . --main-jar "G:\Zinha\coding\java\fdGuiCreator\target\fdGuiCreator-0.jar" --main-class "G:\Zinha\coding\java\fdGuiCreator\src\main\java\fdgc\Main.java" --module-path G:\Zinha\coding\java\fdGuiCreator\target\lib --add-modules javafx.controls,javafx.fxml --win-shortcut --win-menu --name "My App" --jlink-options --bind-services
     }
 }
