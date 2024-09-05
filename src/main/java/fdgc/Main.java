63041832d6edb79b0d5970b3d9333deeeeaf4f5b
XaxisPlayz <66885524+Fotohh@users.noreply.github.com>
2024-09-05 16:57:00 -0700

    vv

--- src/main/java/fdgc/Main.java
+++ src/main/java/fdgc/Main.java
@@ -8,6 +8,6 @@ public class Main {
         Window window = new Window();
         window.load(args);
 
-        //jpackage --type exe --app-version "2.0" --input . --dest . --main-jar "G:\Zinha\coding\java\fdGuiCreator\target\fdGuiCreator-0.jar" --main-class "G:\Zinha\coding\java\fdGuiCreator\src\main\java\fdgc\Main.java" --module-path G:\Zinha\coding\java\fdGuiCreator\target\lib --add-modules javafx.controls,javafx.fxml --win-shortcut --win-menu --name "My App" --jlink-options --bind-services
+        //jpackage --type exe --app-version "2.0" --input G:\Zinha\coding\java\fdGuiCreator\target\jar --dest . --main-jar "G:\Zinha\coding\java\fdGuiCreator\target\jar\fdGuiCreator-0.jar" --main-class "G:\Zinha\coding\java\fdGuiCreator\src\main\java\fdgc\Main.java" --module-path G:\Zinha\coding\java\fdGuiCreator\target\lib --add-modules javafx.controls,javafx.fxml --win-shortcut --win-menu --name "My App" --jlink-options --bind-services
     }
 }
